c7596e3574d4f2814996ee2879c4654cbdf23bfd
t7snow <tuckerwsnow@gmail.com>
2025-03-17 12:53:06 -0400

    fretbaord idea

---
+++ src/components/common/Fretboard.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect, useRef } from "react";
+import { YIN } from 'pitchfinder';
+
+const Fretboard = () => {
+  
+
+  return (
+    <div></div>
+  );
+
+}
+
+//entire guitar fretboard. Should then be split into pieces to use for scales. 
